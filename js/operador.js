cbd13e0bc28e1b0bb34e62187a9170c767c48a9b
franciscorivera2024 <129077203+franciscorivera2024@users.noreply.github.com>
2023-04-24 09:26:29 -0500

    main

---
+++ js/operador.js
@@ -0,0 +1,5 @@
+var firstnumber, secondnumber, result;
+firstnumber= promt("introduzca valor en grados celsius");
+firsnumber= parsefloat(firstnumber);
+secondnumber= 9/5*firsnumber+32;
+document.write("la conversion a farenheit es: ", secondnumber);
